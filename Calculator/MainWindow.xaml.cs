07442c9eae59f252970648d3c411547822bea94b
212301 <212301@DESKTOP-4SFIC8R>
2025-06-24 10:38:05 +0300

    Добавьте файлы проекта.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public double firstNumber;
+        public double secondNumber;
+        public string operation;
+        public bool operationClick;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SevenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void EightBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NineBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FourBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FiveBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SixBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OneBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void TwoBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ThreeBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ZeroBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void DivideBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MultiplyBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MinusBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void PlusBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void EqualsBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
